ddbc72201234e4b1cf40eabaafc103a48f45c70e
GabsMeloTI <rm550202@fiap.com.br>
2024-10-21 14:33:52 -0300

    fix: add comment model.

---
+++ internal/model/comment.go
@@ -0,0 +1,11 @@
+package model
+
+type CommentResponse struct {
+	ID        int64  `json:"id"`
+	Name      string `json:"name"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Bio       string `json:"bio"`
+	AvatarUrl string `json:"avatar_url"`
+}
