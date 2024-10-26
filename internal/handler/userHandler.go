e2f32a928565dd020c99118ad50a0d9a9d8b9a12
GabsMeloTI <rm550202@fiap.com.br>
2024-10-26 19:26:13 -0300

    fix: functions from handler added.

--- internal/handler/userHandler.go
+++ internal/handler/userHandler.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"treads/internal/model"
 	"treads/internal/service"
+	"treads/validation"
 )
 
 type User struct {
@@ -13,6 +14,68 @@ type User struct {
 
 func NewUser(UserInterface service.UserInterface) *User {
 	return &User{UserInterface}
+}
+
+func (h *User) CreateUser(c echo.Context) error {
+	var request model.UserCreateDto
+	if err := c.Bind(&request); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	data := model.UserCreateDto{
+		Name:      request.Name,
+		Username:  request.Username,
+		Email:     request.Email,
+		Password:  request.Password,
+		Bio:       request.Bio,
+		AvatarUrl: request.AvatarUrl,
+	}
+
+	result, err := h.UserInterface.CreateUser(c.Request().Context(), data)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, result)
+}
+
+func (h *User) UpdateUser(c echo.Context) error {
+	var request model.UserUpdateDto
+	if err := c.Bind(&request); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	data := model.UserUpdateDto{
+		ID:        request.ID,
+		Name:      request.Name,
+		Username:  request.Username,
+		Email:     request.Email,
+		Password:  request.Password,
+		Bio:       request.Bio,
+		AvatarUrl: request.AvatarUrl,
+	}
+
+	result, err := h.UserInterface.UpdateUser(c.Request().Context(), data)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, result)
+}
+
+func (h *User) DeleteUser(c echo.Context) error {
+	idStr := c.Param("id")
+	id, err := validation.ParseStringToInt64(idStr)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	err = h.UserInterface.DeleteUser(c.Request().Context(), id)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, "Usuário deletado com sucesso!")
 }
 
 func (h *User) GetAllUsers(c echo.Context) error {
