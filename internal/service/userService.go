1063c3c36e9e2f473ee3081a92a7eb14b0334644
GabsMeloTI <rm550202@fiap.com.br>
2024-10-26 19:25:50 -0300

    fix: function from service added.

--- internal/service/userService.go
+++ internal/service/userService.go
@@ -2,14 +2,17 @@ package service
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"treads/internal/model"
 	"treads/internal/repository"
+	"treads/pkg/crypt"
 )
 
 type UserInterface interface {
-	//CreateUser(context.Context, model.UserCreateDto) (model.UserRespose, error)
-	//UpdateUser(context.Context, model.UserUpdateDto) (model.UserRespose, error)
-	//DeleteUser(context.Context, int32) error
+	CreateUser(context.Context, model.UserCreateDto) (model.UserResponse, error)
+	UpdateUser(context.Context, model.UserUpdateDto) (model.UserResponse, error)
+	DeleteUser(context.Context, int64) error
 	GetAllUsers(context.Context) ([]model.UserResponse, error)
 }
 
@@ -19,6 +22,95 @@ type User struct {
 
 func NewUser(UserInterface repository.UserInterface) *User {
 	return &User{UserInterface: UserInterface}
+}
+
+func (s *User) CreateUser(ctx context.Context, data model.UserCreateDto) (model.UserResponse, error) {
+	arg := data.ParseCreateToUser()
+
+	existsUsername, err := s.UserInterface.GetUsersByName(ctx, data.Username)
+	if existsUsername {
+		return model.UserResponse{}, errors.New("Username already exists")
+	}
+	if err != nil {
+		return model.UserResponse{}, err
+	}
+
+	existsEmail, err := s.UserInterface.GetUsersByEmail(ctx, data.Email)
+	if existsEmail {
+		return model.UserResponse{}, errors.New("Email already exists")
+	}
+	if err != nil {
+		return model.UserResponse{}, err
+	}
+
+	EncryptedPass, err := crypt.HashPassword(data.Password)
+	if err != nil {
+		return model.UserResponse{}, err
+	}
+	result, err := s.UserInterface.CreateUser(ctx, arg)
+	result.Password = EncryptedPass
+	if err != nil {
+		return model.UserResponse{}, err
+	}
+
+	createUser := model.UserResponse{}
+	createUser.ParseFromUserObject(result)
+
+	return createUser, nil
+}
+
+func (s *User) UpdateUser(ctx context.Context, data model.UserUpdateDto) (model.UserResponse, error) {
+	arg := data.ParseUpdateToUser()
+
+	getResult, err := s.UserInterface.GetUserById(ctx, data.ID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return model.UserResponse{}, errors.New("User não encontrado")
+		}
+		return model.UserResponse{}, err
+	}
+
+	if getResult.Username != data.Username || getResult.Email != data.Email {
+		existsUsername, err := s.UserInterface.GetUsersByName(ctx, data.Username)
+		if existsUsername {
+			return model.UserResponse{}, errors.New("Username already exists")
+		}
+		if err != nil {
+			return model.UserResponse{}, err
+		}
+
+		existsEmail, err := s.UserInterface.GetUsersByEmail(ctx, data.Email)
+		if existsEmail {
+			return model.UserResponse{}, errors.New("Email already exists")
+		}
+		if err != nil {
+			return model.UserResponse{}, err
+		}
+	}
+
+	EncryptedPass, err := crypt.HashPassword(data.Password)
+	result, err := s.UserInterface.UpdateUser(ctx, arg)
+	result.Password = EncryptedPass
+	if err != nil {
+		return model.UserResponse{}, err
+	}
+
+	updateUser := model.UserResponse{}
+	updateUser.ParseFromUserObject(result)
+
+	return updateUser, nil
+}
+
+func (s *User) DeleteUser(ctx context.Context, id int64) error {
+	_, err := s.UserInterface.GetUserById(ctx, id)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return errors.New("User não encontrado")
+		}
+		return err
+	}
+
+	return s.UserInterface.DeleteUser(ctx, id)
 }
 
 func (s *User) GetAllUsers(ctx context.Context) ([]model.UserResponse, error) {
