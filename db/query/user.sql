595f0e6d4e47baec55133686728779261b35aa4c
GabsMeloTI <rm550202@fiap.com.br>
2024-10-26 19:23:46 -0300

    fix: added query's necessary,

--- db/query/user.sql
+++ db/query/user.sql
@@ -1,11 +1,11 @@
 -- name: CreateUser :one
-INSERT INTO public."User" (id, "name", username, email, "password", bio, avatar_url, created_at, last_login, active)
-VALUES(nextval('"User_id_seq"'::regclass), $1, $2, $3, $4, $5, $6, now(), $2, true)
+INSERT INTO public."User" (id, "name", username, email, "password", bio, avatar_url, created_at, active)
+VALUES(nextval('"User_id_seq"'::regclass), $1, $2, $3, $4, $5, $6, now(), true)
     RETURNING *;
 
 -- name: UpdateUser :one
 UPDATE "User"
-SET "name"=$2, email=$3, "password"=$4, bio=$5, avatar_url=$6
+SET "name"=$2, email=$3, username=$4, "password"=$5, bio=$6, avatar_url=$7
 WHERE id=$1
     RETURNING *;
 
@@ -14,6 +14,42 @@ UPDATE "User"
 SET active=false
 WHERE id=$1;
 
+-- name: GetUsersLoginByEmail :one
+SELECT *
+FROM public."User"
+WHERE active = true and email = $1 and "password" = $2;
+
+-- name: GetUsersLoginByUsername :one
+SELECT *
+FROM public."User"
+WHERE active = true and username = $1 and "password" = $2;
+
 -- name: GetAllUsers :many
 SELECT *
-FROM "User";
+FROM "User"
+WHERE active = true;
+
+-- name: GetUserById :one
+SELECT *
+FROM public."User"
+WHERE
+    active = true AND
+    id = $1;
+
+-- name: GetUsersByUsername :one
+SELECT EXISTS(
+    SELECT *
+    FROM public."User"
+    WHERE
+        active = true and
+        username = $1
+);
+
+-- name: GetUsersByEmail :one
+SELECT EXISTS(
+    SELECT *
+    FROM public."User"
+    WHERE
+        active = true and
+        "name" = $1
+);
