594126bf22deb4af3d3219f62b12fa573ce3c807
GabsMeloTI <rm550202@fiap.com.br>
2024-10-26 19:24:02 -0300

    fix: sqlc generated.

--- db/sqlc/user.sql.go
+++ db/sqlc/user.sql.go
@@ -11,8 +11,8 @@ import (
 )
 
 const createUser = `-- name: CreateUser :one
-INSERT INTO public."User" (id, "name", username, email, "password", bio, avatar_url, created_at, last_login, active)
-VALUES(nextval('"User_id_seq"'::regclass), $1, $2, $3, $4, $5, $6, now(), $2, true)
+INSERT INTO public."User" (id, "name", username, email, "password", bio, avatar_url, created_at, active)
+VALUES(nextval('"User_id_seq"'::regclass), $1, $2, $3, $4, $5, $6, now(), true)
     RETURNING id, name, username, email, password, bio, avatar_url, active, created_at, last_login
 `
 
@@ -64,6 +64,7 @@ func (q *Queries) DeleteUser(ctx context.Context, id int64) error {
 const getAllUsers = `-- name: GetAllUsers :many
 SELECT id, name, username, email, password, bio, avatar_url, active, created_at, last_login
 FROM "User"
+WHERE active = true
 `
 
 func (q *Queries) GetAllUsers(ctx context.Context) ([]User, error) {
@@ -100,9 +101,127 @@ func (q *Queries) GetAllUsers(ctx context.Context) ([]User, error) {
 	return items, nil
 }
 
+const getUserById = `-- name: GetUserById :one
+SELECT id, name, username, email, password, bio, avatar_url, active, created_at, last_login
+FROM public."User"
+WHERE
+    active = true AND
+    id = $1
+`
+
+func (q *Queries) GetUserById(ctx context.Context, id int64) (User, error) {
+	row := q.db.QueryRowContext(ctx, getUserById, id)
+	var i User
+	err := row.Scan(
+		&i.ID,
+		&i.Name,
+		&i.Username,
+		&i.Email,
+		&i.Password,
+		&i.Bio,
+		&i.AvatarUrl,
+		&i.Active,
+		&i.CreatedAt,
+		&i.LastLogin,
+	)
+	return i, err
+}
+
+const getUsersByEmail = `-- name: GetUsersByEmail :one
+SELECT EXISTS(
+    SELECT id, name, username, email, password, bio, avatar_url, active, created_at, last_login
+    FROM public."User"
+    WHERE
+        active = true and
+        "name" = $1
+)
+`
+
+func (q *Queries) GetUsersByEmail(ctx context.Context, name string) (bool, error) {
+	row := q.db.QueryRowContext(ctx, getUsersByEmail, name)
+	var exists bool
+	err := row.Scan(&exists)
+	return exists, err
+}
+
+const getUsersByUsername = `-- name: GetUsersByUsername :one
+SELECT EXISTS(
+    SELECT id, name, username, email, password, bio, avatar_url, active, created_at, last_login
+    FROM public."User"
+    WHERE
+        active = true and
+        username = $1
+)
+`
+
+func (q *Queries) GetUsersByUsername(ctx context.Context, username string) (bool, error) {
+	row := q.db.QueryRowContext(ctx, getUsersByUsername, username)
+	var exists bool
+	err := row.Scan(&exists)
+	return exists, err
+}
+
+const getUsersLoginByEmail = `-- name: GetUsersLoginByEmail :one
+SELECT id, name, username, email, password, bio, avatar_url, active, created_at, last_login
+FROM public."User"
+WHERE active = true and email = $1 and "password" = $2
+`
+
+type GetUsersLoginByEmailParams struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (q *Queries) GetUsersLoginByEmail(ctx context.Context, arg GetUsersLoginByEmailParams) (User, error) {
+	row := q.db.QueryRowContext(ctx, getUsersLoginByEmail, arg.Email, arg.Password)
+	var i User
+	err := row.Scan(
+		&i.ID,
+		&i.Name,
+		&i.Username,
+		&i.Email,
+		&i.Password,
+		&i.Bio,
+		&i.AvatarUrl,
+		&i.Active,
+		&i.CreatedAt,
+		&i.LastLogin,
+	)
+	return i, err
+}
+
+const getUsersLoginByUsername = `-- name: GetUsersLoginByUsername :one
+SELECT id, name, username, email, password, bio, avatar_url, active, created_at, last_login
+FROM public."User"
+WHERE active = true and username = $1 and "password" = $2
+`
+
+type GetUsersLoginByUsernameParams struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func (q *Queries) GetUsersLoginByUsername(ctx context.Context, arg GetUsersLoginByUsernameParams) (User, error) {
+	row := q.db.QueryRowContext(ctx, getUsersLoginByUsername, arg.Username, arg.Password)
+	var i User
+	err := row.Scan(
+		&i.ID,
+		&i.Name,
+		&i.Username,
+		&i.Email,
+		&i.Password,
+		&i.Bio,
+		&i.AvatarUrl,
+		&i.Active,
+		&i.CreatedAt,
+		&i.LastLogin,
+	)
+	return i, err
+}
+
 const updateUser = `-- name: UpdateUser :one
 UPDATE "User"
-SET "name"=$2, email=$3, "password"=$4, bio=$5, avatar_url=$6
+SET "name"=$2, email=$3, username=$4, "password"=$5, bio=$6, avatar_url=$7
 WHERE id=$1
     RETURNING id, name, username, email, password, bio, avatar_url, active, created_at, last_login
 `
@@ -111,6 +230,7 @@ type UpdateUserParams struct {
 	ID        int64          `json:"id"`
 	Name      string         `json:"name"`
 	Email     string         `json:"email"`
+	Username  string         `json:"username"`
 	Password  string         `json:"password"`
 	Bio       sql.NullString `json:"bio"`
 	AvatarUrl sql.NullString `json:"avatar_url"`
@@ -121,6 +241,7 @@ func (q *Queries) UpdateUser(ctx context.Context, arg UpdateUserParams) (User, e
 		arg.ID,
 		arg.Name,
 		arg.Email,
+		arg.Username,
 		arg.Password,
 		arg.Bio,
 		arg.AvatarUrl,
