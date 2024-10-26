ca971584011f5e3c7c5965491e432dfc179fbf54
GabsMeloTI <rm550202@fiap.com.br>
2024-10-26 19:26:05 -0300

    fix: functions from repository added.

--- internal/repository/userRepository.go
+++ internal/repository/userRepository.go
@@ -11,6 +11,11 @@ type UserInterface interface {
 	CreateUser(context.Context, db.CreateUserParams) (db.User, error)
 	UpdateUser(context.Context, db.UpdateUserParams) (db.User, error)
 	DeleteUser(context.Context, int64) error
+	GetUsersLoginByEmail(context.Context, db.GetUsersLoginByEmailParams) (db.User, error)
+	GetUsersLoginByUsername(context.Context, db.GetUsersLoginByUsernameParams) (db.User, error)
+	GetUserById(context.Context, int64) (db.User, error)
+	GetUsersByName(context.Context, string) (bool, error)
+	GetUsersByEmail(context.Context, string) (bool, error)
 }
 
 type User struct {
@@ -42,4 +47,24 @@ func (r *User) UpdateUser(ctx context.Context, arg db.UpdateUserParams) (db.User
 
 func (r *User) DeleteUser(ctx context.Context, id int64) error {
 	return r.Queries.DeleteUser(ctx, id)
+}
+
+func (r *User) GetUsersLoginByEmail(ctx context.Context, arg db.GetUsersLoginByEmailParams) (db.User, error) {
+	return r.Queries.GetUsersLoginByEmail(ctx, arg)
+}
+
+func (r *User) GetUsersLoginByUsername(ctx context.Context, arg db.GetUsersLoginByUsernameParams) (db.User, error) {
+	return r.Queries.GetUsersLoginByUsername(ctx, arg)
+}
+
+func (r *User) GetUserById(ctx context.Context, id int64) (db.User, error) {
+	return r.Queries.GetUserById(ctx, id)
+}
+
+func (r *User) GetUsersByName(ctx context.Context, name string) (bool, error) {
+	return r.Queries.GetUsersByUsername(ctx, name)
+}
+
+func (r *User) GetUsersByEmail(ctx context.Context, email string) (bool, error) {
+	return r.Queries.GetUsersByEmail(ctx, email)
 }
