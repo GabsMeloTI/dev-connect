1ef4eab6e750b82c8ce7b35618544034e685e0d6
GabsMeloTI <rm550202@fiap.com.br>
2024-10-28 13:21:43 -0300

    fix: query updated.

--- db/migration/000001_create_table_user.up.sql
+++ db/migration/000001_create_table_user.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "User" (
                         id BIGSERIAL PRIMARY KEY,
                         name VARCHAR(255) NOT NULL,
-                        username VARCHAR(255) UNIQUE NOT NULL,
-                        email VARCHAR(255) UNIQUE NOT NULL,
+                        username VARCHAR(255) NOT NULL,
+                        email VARCHAR(255) NOT NULL,
                         password VARCHAR(255) NOT NULL,
                         bio VARCHAR,
                         avatar_url VARCHAR,
